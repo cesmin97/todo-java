101ea32b833d9e8089257749da14f3b1645e6666
cesmin97 <cesmin1216@gmail.com>
2024-12-20 17:25:23 +0900

    User 모델 어노테이션 변경

--- src/main/java/com/cesmin/todo/user/model/User.java
+++ src/main/java/com/cesmin/todo/user/model/User.java
@@ -28,27 +28,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @NotBlank(message = "Title is required")
-    @Size(max = 20, message = "Title cannot be longer than 20 characters")
-    @Column(unique = true)
+    @Column(nullable = false, length = 20, unique = true)
     private String username;
 
-    @NotBlank(message = "Contents is required")
-    @Size(max = 255, message = "Contents cannot be longer than 255 characters")
+    @Column(nullable = false, length = 255)
     private String password;
 
-    @NotBlank(message = "Name is required")
-    @Size(max = 50, message = "Name cannot be longer than 255 characters")
+    @Column(nullable = false, length = 50)
     private String name;
 
-    @NotBlank(message = "Phone is required")
-    @Size(max = 20, message = "Phone cannot be longer than 20 characters")
+    @Column(nullable = false, length = 20)
     private String phone;
 
-    @Size(max = 50, message = "Email cannot be longer than 50 characters")
+    @Column(length = 50)
     private String email;
 
-    @NotNull
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
